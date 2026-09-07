c815d1dafac44d886cca2cde901310e5a6935555
hix-001 <harsh.25scs1003004095@iilm.edu>
2026-09-07 18:27:23 +0530

    Add length check and abbreviation formatting for strings

---
+++ CPP/Strings/71A_WayTooLongWords.cpp
@@ -0,0 +1,27 @@
+//07/09/2026
+//Codeforces 71A - Abbreviate words strictly longer than 10 characters
+//07/09/2026
+//Codeforces 71A - Abbreviate words strictly longer than 10 characters
+ 
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n;
+    if (cin >> n) {
+        while (n--) {
+            string s;
+            cin >> s;
+            int len = s.length();
+            
+            if (len > 10) {
+                cout << s[0] << len - 2 << s[len - 1] << "\n";
+            } else {
+                cout << s << "\n";
+            }
+        }
+    }
+    return 0;
+}

847cc4918b05e3857acfcfb3c778a1f8abc84e7f
hix-001 <harsh.25scs1003004095@iilm.edu>
2026-09-09 15:08:07 +0530

    Implement adjacent swap condition check for sorting

---
+++ CPP/Sorting/1903A_HalloumiBoxes.cpp
@@ -0,0 +1,37 @@
+//09/09/2026
+//Codeforces 1903A - Can the array be sorted using subarray reversals of length at most k?
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+void solve() {
+    int n, k;
+    cin >> n >> k;
+    vector<int> a(n);
+    for (int i = 0; i < n; ++i) {
+        cin >> a[i];
+    }
+    
+    if (k >= 2 || is_sorted(a.begin(), a.end())) {
+        cout << "YES\n";
+    } else {
+        cout << "NO\n";
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int t;
+    if (cin >> t) {
+        while (t--) {
+            solve();
+        }
+    }
+    
+    return 0;
+}

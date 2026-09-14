590d281ef2c575158f3b3bc7ccf6edcce1745626
hix-001 <harsh.25scs1003004095@iilm.edu>
2026-09-14 18:39:54 +0530

    Optimize step calculation using integer ceiling division and C-style IO

---
+++ CPP/Math/617A_Elephant.cpp
@@ -0,0 +1,11 @@
+//14/09/2026
+//Codeforces 617A - Calculate minimum steps using integer ceiling division
+
+#include <cstdio>
+int main() {
+    int x;
+    if (scanf("%d", &x) == 1) {
+        printf("%d\n", (x + 4) / 5);
+    }
+    return 0;
+}
